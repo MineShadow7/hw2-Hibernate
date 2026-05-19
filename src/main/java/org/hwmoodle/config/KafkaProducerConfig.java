b0856d9877038c2f3ce6115f71eab1e18db82b5c
MineShadow7 <shad-karagodin@ya.ru>
2026-05-19 17:08:06 +0300

    Fix producer config, resolved dependencies, agregate docker images, hardcode variables

--- src/main/java/org/hwmoodle/config/KafkaProducerConfig.java
+++ src/main/java/org/hwmoodle/config/KafkaProducerConfig.java
@@ -1,47 +1,35 @@
 package org.hwmoodle.config;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.hwmoodle.core.dto.UserNotificationEvent;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
 
 @Configuration
 @EnableKafka
-@ConditionalOnProperty(
-        name = "spring.kafka.bootstrap-servers",
-        havingValue = "localhost:9092",
-        matchIfMissing = true
-)
 public class KafkaProducerConfig {
-
-    @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
-    private String bootstrapServers;
-
     @Bean
-    public ProducerFactory<String, UserNotificationEvent> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
+    public ProducerFactory<String, UserNotificationEvent> userEventProducerFactory(
+            @Value("${spring.kafka.bootstrap-servers}") String bootstrapServers) {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(props);
     }
 
     @Bean
-    public KafkaTemplate<String, UserNotificationEvent> kafkaTemplate(
-            ProducerFactory<String, UserNotificationEvent> producerFactory) {
-        return new KafkaTemplate<>(producerFactory);
+    public KafkaTemplate<String, UserNotificationEvent> userEventKafkaTemplate(
+            ProducerFactory<String, UserNotificationEvent> userEventProducerFactory) {
+        return new KafkaTemplate<>(userEventProducerFactory);
     }
 }
-
